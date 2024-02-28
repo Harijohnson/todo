dc3cfc538e15cc54b1d42486ecaacf268f379980
Harijohnson <harijn72@gmail.com>
2024-02-28 11:27:22 +0530

    table error fixed

--- todo_frontend/src/screens/HomeScreen.jsx
+++ todo_frontend/src/screens/HomeScreen.jsx
@@ -1,133 +1,4 @@
-// import React, { useEffect } from 'react'
-// import {
-//   MDBBtn,
-//   MDBCard,
-//   MDBCardBody,
-//   MDBCol,
-//   MDBContainer,
-//   MDBInput,
-//   MDBRow,
-//   MDBTable,
-//   MDBTableBody,
-//   MDBTableHead,
-// } from "mdb-react-ui-kit";
-// import { useSelector } from 'react-redux'
-// import { useDispatch } from 'react-redux'
-// import { todoItem } from '../actions/TodoAction'
-// function HomeScreen() {
-
-//   const dispatch = useDispatch()
-//   const userLogin  = useSelector(state => state.userInfo)   
-//   const { userInfo } = userLogin
-
-//   useEffect(() => {
-//     dispatch(todoItem());
-//   }, [dispatch]);
-
-
-  
-//   const todoListItem  = useSelector(state => state.todoList)   
-//   const { todoList } = todoListItem
-//   console.log('todo item object' ,todoList)
-
-
-
-//   // console.log('usee name',userInfo.name)
-
-
-
-
-
-//   return (
-//     <section className="vh-100" style={{ backgroundColor: "#eee" }}>
-//     <MDBContainer className="py-5 h-100">
-//       <MDBRow className="d-flex justify-content-center align-items-center">
-//         <MDBCol lg="9" xl="7">
-//           <MDBCard className="rounded-3">
-//             <MDBCardBody className="p-4">
-//               <h4 className="text-center my-3 pb-3">To Do App</h4>
-//               <MDBRow className="row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2">
-//                 <MDBCol size="12">
-//                   <MDBInput
-//                     label="Enter a task here"
-//                     id="form1"
-//                     type="text"
-//                   />
-//                 </MDBCol>
-//                 <MDBCol size="12">
-//                   <MDBBtn type="submit">Save</MDBBtn>
-//                 </MDBCol>
-//                 <MDBCol size="12">
-//                   <MDBBtn type="submit" color="warning">
-//                     Get tasks
-//                   </MDBBtn>
-//                 </MDBCol>
-//               </MDBRow>
-//               <MDBTable className="mb-4">
-//                 <MDBTableHead>
-//                   <tr>
-//                     <th scope="col">No.</th>
-//                     <th scope="col">Todo item</th>
-//                     <th scope="col">Status</th>
-//                     <th scope="col">Actions</th>
-//                   </tr>
-//                 </MDBTableHead>
-//                 <MDBTableBody>
-//                   <tr>
-//                     <th scope="row">1</th>
-//                     <td>week</td>
-//                     <td>In progress</td>
-//                     <td>
-//                       <MDBBtn type="submit" color="danger">
-//                         Delete
-//                       </MDBBtn>
-
-//                       <MDBBtn type="submit" color="success" className="ms-1">
-//                         Finished
-//                       </MDBBtn>
-//                     </td>
-//                   </tr>
-//                   <tr>
-//                     <th scope="row">2</th>
-//                     <td>Renew car insurance</td>
-//                     <td>In progress</td>
-//                     <td>
-//                       <MDBBtn type="submit" color="danger">
-//                         Delete
-//                       </MDBBtn>
-
-//                       <MDBBtn type="submit" color="success" className="ms-1">
-//                         Finished
-//                       </MDBBtn>
-//                     </td>
-//                   </tr>
-//                   <tr>
-//                     <th scope="row">3</th>
-//                     <td>Sign up for online course</td>
-//                     <td>In progress</td>
-//                     <td>
-//                       <MDBBtn type="submit" color="danger">
-//                         Delete
-//                       </MDBBtn>
-
-//                       <MDBBtn type="submit" color="success" className="ms-1">
-//                         Finished
-//                       </MDBBtn>
-//                     </td>
-//                   </tr>
-//                 </MDBTableBody>
-//               </MDBTable>
-//             </MDBCardBody>
-//           </MDBCard>
-//         </MDBCol>
-//       </MDBRow>
-//     </MDBContainer>
-//   </section>
-//   )
-// }
-
-// export default HomeScreen
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 import {
   MDBBtn,
   MDBCard,
@@ -140,97 +11,98 @@ import {
   MDBTableBody,
   MDBTableHead,
 } from "mdb-react-ui-kit";
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { todoItem } from '../actions/TodoAction';
-import Loading from '../components/Loading'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { todoItem } from '../actions/TodoAction'
 function HomeScreen() {
-  const dispatch = useDispatch();
-  const userLogin = useSelector(state => state.userInfo);
-  const { userInfo } = userLogin;
+
+  const dispatch = useDispatch()
+  const userLogin  = useSelector(state => state.userInfo)   
+  const { userInfo } = userLogin
 
   useEffect(() => {
-    // Check if user information is stored in local storage
-    const storedUserInfo = localStorage.getItem('userInfo');
-    if (storedUserInfo) {
-      // If user information is found in local storage, parse and set it in the Redux store
-      dispatch({ type: 'USER_LOGIN_SUCCESS', payload: JSON.parse(storedUserInfo) });
-    } else {
-      // If no user information is found in local storage, dispatch todoItem action to fetch todo list
-      dispatch(todoItem());
-    }
+    dispatch(todoItem());
   }, [dispatch]);
 
-  const todoListItem = useSelector(state => state.todoList);
-  const { todoList,loading } = todoListItem;
+
+  
+  const todoListItem  = useSelector(state => state.todoList)   
+  const { todoList } = todoListItem
+  console.log('todo item object' ,todoList)
+
+
+
+  // console.log('usee name',userInfo.name)
+
+
+
+
 
   return (
     <section className="vh-100" style={{ backgroundColor: "#eee" }}>
-      { loading ? (<Loading />):
-      <MDBContainer className="py-5 h-100">
-        <MDBRow className="d-flex justify-content-center align-items-center">
-          <MDBCol lg="9" xl="7">
-            <MDBCard className="rounded-3">
-              <MDBCardBody className="p-4">
-                {userInfo ? (
-                  <>
-                    <h4 className="text-center my-3 pb-3">To Do App</h4>
-                    <MDBRow className="row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2">
-                      <MDBCol size="12">
-                        <MDBInput
-                          label="Enter a task here"
-                          id="form1"
-                          type="text"
-                        />
-                      </MDBCol>
-                      <MDBCol size="12">
-                        <MDBBtn type="submit">Save</MDBBtn>
-                      </MDBCol>
-                      <MDBCol size="12">
-                        <MDBBtn type="submit" color="warning">
-                          Get tasks
-                        </MDBBtn>
-                      </MDBCol>
-                    </MDBRow>
-                    <MDBTable className="mb-4">
-                      <MDBTableHead>
-                        <tr>
-                          <th scope="col">No.</th>
-                          <th scope="col">Todo item</th>
-                          <th scope="col">Status</th>
-                          <th scope="col">Actions</th>
-                        </tr>
-                      </MDBTableHead>
-                      <MDBTableBody>
-                        {todoList.map((todo, index) => (
-                          <tr key={index}>
-                            <th scope="row">{index + 1}</th>
-                            <td>{todo.todo}</td>
-                            <td>In progress</td>
-                            <td>
-                              <MDBBtn type="submit" color="danger">
-                                Delete
-                              </MDBBtn>
-                              <MDBBtn type="submit" color="success" className="ms-1">
-                                Finished
-                              </MDBBtn>
-                            </td>
-                          </tr>
-                        ))}
-                      </MDBTableBody>
-                    </MDBTable>
-                  </>
-                ) : (
-                  <h4 className="text-center my-3 pb-3">Welcome to the To Do App</h4>
-                )}
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-      }
-    </section>
-  );
+    <MDBContainer className="py-5 h-100">
+      <MDBRow className="d-flex justify-content-center align-items-center">
+        <MDBCol lg="9" xl="7">
+          <MDBCard className="rounded-3">
+            <MDBCardBody className="p-4">
+              <h4 className="text-center my-3 pb-3">To Do App</h4>
+              <MDBRow className="row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2">
+                <MDBCol size="12">
+                  <MDBInput
+                    label="Enter a task here"
+                    id="form1"
+                    type="text"
+                  />
+                </MDBCol>
+                <MDBCol size="12">
+                  <MDBBtn type="submit">Save</MDBBtn>
+                </MDBCol>
+                <MDBCol size="12">
+                  <MDBBtn type="submit" color="warning">
+                    Get tasks
+                  </MDBBtn>
+                </MDBCol>
+              </MDBRow>
+
+              {/* Conditional rendering based on user login */}
+              {userInfo ? (
+                <MDBTable className="mb-4">
+                  <MDBTableHead>
+                    <tr>
+                      <th scope="col">No.</th>
+                      <th scope="col">Todo item</th>
+                      <th scope="col">Status</th>
+                      <th scope="col">Actions</th>
+                    </tr>
+                  </MDBTableHead>
+                  <MDBTableBody>
+                    {todoList.map((todo, index) => (
+                      <tr key={index}>
+                        <th scope="row">{index + 1}</th>
+                        <td>{todo.todo}</td>
+                        <td>{todo.status || 'NA'}</td>
+                        <td>
+                          <MDBBtn type="submit" color="danger">
+                            Delete
+                          </MDBBtn>
+                          <MDBBtn type="submit" color="success" className="ms-1">
+                            Finished
+                          </MDBBtn>
+                        </td>
+                      </tr>
+                    ))}
+                  </MDBTableBody>
+                </MDBTable>
+              ) : (
+                <h2>Welcome to Todo App. Please login to view todos.</h2>
+              )}
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  </section>
+  )
 }
 
-export default HomeScreen;
+export default HomeScreen
